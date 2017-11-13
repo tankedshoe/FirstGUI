4d7e426b533172d66c1c0c87a15ae473011a4b4b
tankedshoe <tankedshoe@gmail.com>
2017-11-13 08:06:03 -0700

    Here is some more stuff I forgot to save that should have been in the last commit. sorry

--- src/gui/controller/GUIAppRunner.java
+++ src/gui/controller/GUIAppRunner.java
@@ -2,5 +2,9 @@ package gui.controller;
 
 public class GUIAppRunner
 {
-
+	public static void main(String [] args)
+	{
+		GUIAppController myApp = new GUIAppController();
+		myApp.start();
+	}
 }
--- src/gui/view/GUIFrame.java
+++ src/gui/view/GUIFrame.java
@@ -1,6 +1,24 @@
 package gui.view;
 
-public class GUIFrame
-{
+import gui.controller.GUIAppController;
+import javax.swing.JFrame;
 
+public class GUIFrame extends JFrame
+{
+	private GUIAppController appController;
+	
+	public GUIFrame(GUIAppController appController)
+	{
+		super();
+		this.appController = appController;
+		
+		setupFrame();
+	}
+	
+	private void setupFrame()
+	{
+		this.setTitle("The window title");
+		this.setSize(500, 500);
+		this.setVisible(true);
+	}
 }
