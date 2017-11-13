9a5080c83f254876ce9bdc89fe3fad72ab674700
tankedshoe <tankedshoe@gmail.com>
2017-11-13 09:07:11 -0700

    Imported some sick awt event action thingys. Not really sure what I'm gonna use them for yet.

--- src/gui/view/GUIPanel.java
+++ src/gui/view/GUIPanel.java
@@ -5,6 +5,8 @@ import javax.swing.JPanel;
 import javax.swing.JButton;
 import java.awt.Color;
 import javax.swing.SpringLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GUIPanel extends JPanel
 {
@@ -21,6 +23,7 @@ public class GUIPanel extends JPanel
 		
 		setupPanel();
 		setupLayout();
+		setupListeners();
 	}
 	
 	/**
