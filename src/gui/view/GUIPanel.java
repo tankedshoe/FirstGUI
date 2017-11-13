7741a345daf0eac71805789036cbd623fc45e8cf
tankedshoe <tankedshoe@gmail.com>
2017-11-13 08:42:59 -0700

    Got rid of junky flow layout, and replaced it with awesome SpringLayout.

--- src/gui/view/GUIPanel.java
+++ src/gui/view/GUIPanel.java
@@ -4,17 +4,47 @@ import gui.controller.GUIAppController;
 import javax.swing.JPanel;
 import javax.swing.JButton;
 import java.awt.Color;
+import javax.swing.SpringLayout;
 
 public class GUIPanel extends JPanel
 {
 	private GUIAppController appController;
 	private JButton firstButton;
+	private SpringLayout baseLayout;
 	
 	public GUIPanel(GUIAppController appController)
 	{
 		super();
 		this.appController = appController;
 		firstButton = new JButton("Wow a friggin button!");
+		baseLayout  = new SpringLayout();
+		
+		setupPanel();
+	}
+	
+	/**
+	 * Used to add all components to the subclass of JPanel.
+	 * This installs them into the panel so they are seen in the GUI.
+	 */
+	private void setupPanel()
+	{
+		this.setLayout(baseLayout);
+		this.add(firstButton);
+	}
+	
+	/**
+	 * This is used to hold all the layout and arrangement code for SpringLayout.
+	 */
+	private void setupLayout()
+	{
+		
+	}
+	
+	/**
+	 * This helper method is used to link any GUI components to the associated listeners.
+	 */
+	private void setupListeners()
+	{
 		
 	}
 }
