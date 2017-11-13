d84e8842ef8d527e1332810236e5b00a48c8cfc9
tankedshoe <tankedshoe@gmail.com>
2017-11-13 08:12:48 -0700

    Hey look my GUI runs now!

--- src/gui/controller/GUIAppController.java
+++ src/gui/controller/GUIAppController.java
@@ -1,7 +1,16 @@
 package gui.controller;
 
+import gui.view.GUIFrame;
+
 public class GUIAppController
 {
+	private GUIFrame appFrame;
+	
+	public GUIAppController()
+	{
+		this.appFrame = new GUIFrame(this);
+	}
+	
 	public void start()
 	{
 		
