b13be6b5a72a059ce42fc8ea50d62cba7ac4431a
tankedshoe <tankedshoe@gmail.com>
2017-11-13 08:22:55 -0700

    Created a panel class with a new friggin button in place.

---
+++ src/gui/view/GUIPanel.java
@@ -0,0 +1,20 @@
+package gui.view;
+
+import gui.controller.GUIAppController;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import java.awt.Color;
+
+public class GUIPanel extends JPanel
+{
+	private GUIAppController appController;
+	private JButton firstButton;
+	
+	public GUIPanel(GUIAppController appController)
+	{
+		super();
+		this.appController = appController;
+		firstButton = new JButton("Wow a friggin button!");
+		
+	}
+}
